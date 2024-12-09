ff1df9e5fb86400d8c2f4afa7e1e905a38fae447
Michael Tan <thr205@hotmail.com>
2024-12-09 22:17:34 +0800

    fix: add style

--- src/components/Graph/Graph.tsx
+++ src/components/Graph/Graph.tsx
@@ -1,121 +1,151 @@
 import React, { CSSProperties, FC, useState } from 'react';
+
 import { Select } from 'antd';
-import { BarChart, LineChart, PieChart, Pie, Line, Bar, Rectangle, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+
+import {
+  Bar,
+  BarChart,
+  CartesianGrid,
+  Legend,
+  Line,
+  LineChart,
+  Pie,
+  PieChart,
+  Rectangle,
+  ResponsiveContainer,
+  Tooltip,
+  XAxis,
+  YAxis,
+} from 'recharts';
 
 enum ChartType {
-    BarChart,
-    PieChart,
-    LineChart
+  BarChart,
+  PieChart,
+  LineChart,
 }
 
 interface GraphProps {
-    data: Record<string, any>;
-    style: CSSProperties;
+  data: Record<string, any>;
+  style?: CSSProperties;
 }
 
 const classPrefix = 'bi-graph';
 
 export const Graph: FC<GraphProps> = (props) => {
-    const [chartType, setChartType] = useState<ChartType>(ChartType.BarChart);
-    const categories = Object.keys(props.data[0]).filter((v) => { return v !== 'name' });
-    const pieChartData = categories.map(() => [] as Record<string, any>[])
-    categories.forEach((category, idx) => {
-        props.data.forEach((bullet :Record<string, any>) => {
-            pieChartData[idx].push({
-                // @ts-ignore
-                name: bullet.name, value: bullet[category]
-            })
-        });
+  const [chartType, setChartType] = useState<ChartType>(ChartType.BarChart);
+  const categories = Object.keys(props.data[0]).filter((v) => {
+    return v !== 'name';
+  });
+  const pieChartData = categories.map(() => [] as Record<string, any>[]);
+  categories.forEach((category, idx) => {
+    props.data.forEach((bullet: Record<string, any>) => {
+      pieChartData[idx].push({
+        // @ts-ignore
+        name: bullet.name,
+        value: bullet[category],
+      });
     });
+  });
 
-    return (
-        <div className={`${classPrefix}`} style={props.style}>
-            <div className={`${classPrefix}-action`}>
-                <Select className={`${classPrefix}-action-select`} style={ { maxWidth: 100 } } defaultValue={ ChartType.BarChart }
-                    onChange={ (value: ChartType) => {
-                        setChartType(value)
-                    } }
-                    options={ [
-                        { label: '柱状图', value: ChartType.BarChart },
-                        { label: '饼状图', value: ChartType.PieChart },
-                        { label: '折线图', value: ChartType.LineChart }
-                    ] }
-                />
-            </div>
-            <div className={`${classPrefix}-graph`}>
-            {
-                chartType === ChartType.BarChart && (
-                    <ResponsiveContainer width="100%" height={ 300 }>
-                        <BarChart
-                            width={ 500 }
-                            height={ 300 }
-                            data={ props.data as any[] }
-                            margin={ {
-                                top: 5,
-                                right: 30,
-                                left: 20,
-                                bottom: 5,
-                            } }
-                        >
-                            <CartesianGrid strokeDasharray="3 3" />
-                            <XAxis dataKey="name" />
-                            <YAxis />
-                            <Tooltip />
-                            <Legend />
-                            <Bar dataKey="pv" fill="#8884d8" activeBar={ <Rectangle fill="pink" stroke="blue" /> } />
-                            <Bar dataKey="uv" fill="#82ca9d" activeBar={ <Rectangle fill="gold" stroke="purple" /> } />
-                        </BarChart>
-                    </ResponsiveContainer>
-                )
-            }
-            {
-                chartType === ChartType.LineChart && (
-                    <ResponsiveContainer width="100%" height={ 300 }>
-                        <LineChart
-                            width={ 500 }
-                            height={ 300 }
-                            data={ props.data as any[] }
-                            margin={ {
-                                top: 5,
-                                right: 30,
-                                left: 20,
-                                bottom: 5,
-                            } }
-                        >
-                            <CartesianGrid strokeDasharray="3 3" />
-                            <XAxis dataKey="name" />
-                            <YAxis />
-                            <Tooltip />
-                            <Legend />
-                            <Line type="monotone" dataKey="pv" stroke="#8884d8" activeDot={ { r: 8 } } />
-                            <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
-                        </LineChart>
-                    </ResponsiveContainer>
-                )
-            }
-            {
-                chartType === ChartType.PieChart && pieChartData.map((v, idx) => {
-                    return (
-                        <ResponsiveContainer width="100%" height={ 300 } key={ idx }>
-                            <PieChart width={ 400 } height={ 400 } >
-                                <Pie
-                                    key={ categories[idx] }
-                                    dataKey="value"
-                                    isAnimationActive={ false }
-                                    data={ v }
-                                    cx="50%"
-                                    cy="50%"
-                                    outerRadius={ 80 }
-                                    fill="#8884d8"
-                                    label
-                                />
-                                <Tooltip />
-                            </PieChart>
-                        </ResponsiveContainer>
-                    )
-                })
-            }
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className={`${classPrefix}`} style={props?.style}>
+      <div className={`${classPrefix}-action`}>
+        <Select
+          className={`${classPrefix}-action-select`}
+          style={{ maxWidth: 100 }}
+          defaultValue={ChartType.BarChart}
+          onChange={(value: ChartType) => {
+            setChartType(value);
+          }}
+          options={[
+            { label: '柱状图', value: ChartType.BarChart },
+            { label: '饼状图', value: ChartType.PieChart },
+            { label: '折线图', value: ChartType.LineChart },
+          ]}
+        />
+      </div>
+      <div className={`${classPrefix}-graph`}>
+        {chartType === ChartType.BarChart && (
+          <ResponsiveContainer width="100%" height={300}>
+            <BarChart
+              width={500}
+              height={300}
+              data={props.data as any[]}
+              margin={{
+                top: 5,
+                right: 30,
+                left: 20,
+                bottom: 5,
+              }}
+            >
+              <CartesianGrid strokeDasharray="3 3" />
+              <XAxis dataKey="name" />
+              <YAxis />
+              <Tooltip />
+              <Legend />
+              <Bar
+                dataKey="pv"
+                fill="#8884d8"
+                activeBar={<Rectangle fill="pink" stroke="blue" />}
+              />
+              <Bar
+                dataKey="uv"
+                fill="#82ca9d"
+                activeBar={<Rectangle fill="gold" stroke="purple" />}
+              />
+            </BarChart>
+          </ResponsiveContainer>
+        )}
+        {chartType === ChartType.LineChart && (
+          <ResponsiveContainer width="100%" height={300}>
+            <LineChart
+              width={500}
+              height={300}
+              data={props.data as any[]}
+              margin={{
+                top: 5,
+                right: 30,
+                left: 20,
+                bottom: 5,
+              }}
+            >
+              <CartesianGrid strokeDasharray="3 3" />
+              <XAxis dataKey="name" />
+              <YAxis />
+              <Tooltip />
+              <Legend />
+              <Line
+                type="monotone"
+                dataKey="pv"
+                stroke="#8884d8"
+                activeDot={{ r: 8 }}
+              />
+              <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
+            </LineChart>
+          </ResponsiveContainer>
+        )}
+        {chartType === ChartType.PieChart &&
+          pieChartData.map((v, idx) => {
+            return (
+              <ResponsiveContainer width="100%" height={300} key={idx}>
+                <PieChart width={400} height={400}>
+                  <Pie
+                    key={categories[idx]}
+                    dataKey="value"
+                    isAnimationActive={false}
+                    data={v}
+                    cx="50%"
+                    cy="50%"
+                    outerRadius={80}
+                    fill="#8884d8"
+                    label
+                  />
+                  <Tooltip />
+                </PieChart>
+              </ResponsiveContainer>
+            );
+          })}
+      </div>
+    </div>
+  );
+};
--- src/components/dataframe/dataframe.tsx
+++ src/components/dataframe/dataframe.tsx
@@ -1,24 +1,32 @@
-import React, { FC } from "react";
-import { Table } from "antd";
+import React, { CSSProperties, FC } from 'react';
+
+import { Table } from 'antd';
 
 interface DataframeProps {
-    data: {
-        dataSource: Array<Record<string, any> & { key: string }>
-        columns: Array<{
-            key: string
-            title: string
-            dataIndex: string
-        }>
-    }
+  data: {
+    dataSource: Array<Record<string, any> & { key: string }>;
+    columns: Array<{
+      key: string;
+      title: string;
+      dataIndex: string;
+    }>;
+  };
+  style?: CSSProperties;
 }
 
 export const Dataframe: FC<DataframeProps> = (props) => {
-    const {
-        data: {
-            columns,
-            dataSource
-        }
-    } = props;
+  const {
+    data: { columns, dataSource },
+    style,
+  } = props;
 
-    return <Table dataSource={dataSource} columns={columns} size="small" pagination={false} />
-}
+  return (
+    <Table
+      size="small"
+      style={style}
+      columns={columns}
+      pagination={false}
+      dataSource={dataSource}
+    />
+  );
+};
